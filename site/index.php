27f02e772b316b636f3dd1ff05ef6b649655e7d8
João Victor Bortolozo <110576032+joaovictorbortolozo@users.noreply.github.com>
2023-04-20 22:19:18 -0300

    final pag inicial

--- site/index.php
+++ site/index.php
@@ -1,35 +1,97 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+include "menu.php";
+include "cabecalho.php";
+?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <title>Igames</title>
-</head>
-<body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">iGames</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                </ul>
-                <form class="d-flex"  action="login.php" method="post">
-                    <input class="form-control me-2" type="email" placeholder="email">
-                    <input class="form-control me-2" type="password" placeholder="Senha">
-                    <button class="btn btn-outline-success" type="submit">E-mail</button>
-                    <button class="btn btn-outline-success" type="submit">Cadastrar</button>
-                </form>
+<div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active" data-bs-interval="3000">
+            <img src="img/forza.png" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item" data-bs-interval="3000">
+            <img src="img/forzam.png" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item" data-bs-interval="3000">
+            <img src="img/halo-infinite.png" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item" data-bs-interval="3000">
+            <img src="img/as.png" class="d-block w-100" alt="...">
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
+<div class="container mt-5">
+    <div class="col-12 text-center">
+        <h1>Jogos em destaque</h1>
+    </div>
+    <div class="col-12 text-center">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid esse suscipit fugit dignissimos, perspiciatis sit eveniet veniam, consectetur reiciendis doloremque sunt, officia distinctio? Corporis eos ad quam, optio impedit nostrum?</p>
+    </div>
+    <div class="row mt-5">
+
+
+        <?php
+        for ($i = 0; $i < 2; $i++) :
+        ?>
+            <div class="col-md-3 text-center">
+                <img src="img/forzam.png" class="img-fluid">
+                <h2 class="mb-1">Forza Motorsport</h2>
+                <a href="nomedojogo.php" class="btn btn-primary mb-2">VER MAIS</a>
+            </div>
+            <div class="col-md-3 text-center">
+                <img src="img/forza.png" class="img-fluid">
+                <h2 class="mb-1">Forza horizon 5</h2>
+                <a href="nomedojogo.php" class="btn btn-primary mb-2">VER MAIS</a>
+            </div>
+
+            <div class="col-md-3 text-center">
+                <img src="img/as.png" class="img-fluid">
+                <h2 class="mb-1">Assanssin's Creed</h2>
+                <a href="nomedojogo.php" class="btn btn-primary mb-2">VER MAIS</a>
+            </div>
+
+            <div class="col-md-3 text-center">
+                <img src="img/halo.png" class="img-fluid">
+                <h2 class="mb-1">Halo 4 </h2>
+                <a href="nomedojogo.php" class="btn btn-primary mb-5">VER MAIS</a>
+            </div>
+        <?php
+        endfor;
+        ?>
+    </div>
+</div>
+<div class="col-12 text-center mb-3">
+    <h2>Entre em contato</h2>
+</div>
+<div class="col-12 text-center">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente aliquid ipsa quos iure distinctio excepturi facilis nisi nemo. Expedita ipsam pariatur, harum dolorum corporis voluptas qui ipsum rerum suscipit officia!</p>
+</div>
+<div class="row">
+    <form action="inserir-contato.php" method="post">
+        <div class="col">
+            <div class="mb-2">
+                <input type="text" class="form-control" Name="nome" placeholder="Nome">
+            </div>
+            <div class="mb-3">
+                <input type="tel" class="form-control" Name="telefone" placeholder="Telefone">
+            </div>
+            <div class="mb-2">
+                <textarea class="form-control" name="duvidas" placeholder="Dúvida" rows="4"></textarea>
+            </div>
+            <div class="mb-2">
+                <button type="submit" class="btn btn-sucesses w-25">
             </div>
         </div>
-    </nav>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    </form>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
+
 </html>
---
+++ site/menu.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">iGames</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                </ul>
+                <form class="d-flex" action="login.php" method="post">
+                    <input class="form-control me-2" type="E-mail" placeholder="E-mail">
+                    <input class="form-control me-2" type="password" placeholder="Senha">
+                    <button class="btn btn-outline-success me-2" type="submit">Entrar</button>
+                    <a href="novo-usuario.php" class="btn btn-outline-primary">Cadastrar</a>
+                </form>
+            </div>
+        </div>
+    </nav>
